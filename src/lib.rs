a5c47a2e38a32573931c375e3a2600f23418753e
akilhylton <hyltonakil@gmail.com>
2022-11-07 18:14:11 -0500

    Modularized library for state indicator

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,54 @@
 #![no_std]
-use cortex_m::delay::Delay;
-use rp2040_hal::Sio;
-use rp2040_hal::gpio::Pins;
-use rp2040_hal::pac;
-use rp2040_hal::Watchdog;
-use rp2040_hal::Clock;
-use rp2040_hal::clocks::init_clocks_and_plls;
 
-/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
-/// if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+pub mod state_model {
+    use cortex_m::delay::Delay;
+    use rp2040_hal::Sio;
+    use rp2040_hal::gpio::Pins;
+    use rp2040_hal::gpio::Pin;
+    use rp2040_hal::gpio::bank0::Gpio25;
+    use rp2040_hal::gpio::PushPullOutput;
+    use rp2040_hal::Watchdog;
+    use rp2040_hal::Clock;
+    use embedded_hal::digital::v2::OutputPin;
+    use rp2040_hal::clocks::init_clocks_and_plls;
+    use rp2040_hal::{pac};
 
-pub fn init () -> (Pins, Delay) {
-    let mut peripherals = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = Watchdog::new(peripherals.WATCHDOG);    
-    // The default is to generate a 125 MHz system clock
-    let clocks =  init_clocks_and_plls(
-            XTAL_FREQ_HZ,
-            peripherals.XOSC,
-            peripherals.CLOCKS,
-            peripherals.PLL_SYS,
-            peripherals.PLL_USB,
-            &mut peripherals.RESETS,
-            &mut watchdog,
-        )
-        .ok()
-        .unwrap();
+    /// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
+    /// if your board has a different frequency
+    const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
-    let sio = Sio::new(peripherals.SIO);
-    let pins = Pins::new(peripherals.IO_BANK0,peripherals.PADS_BANK0,sio.gpio_bank0, &mut peripherals.RESETS);
-    // The delay object lets us wait for specified amounts of time (in milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    return (pins, delay);
+    const DELAY_LEN: u32 = 150;
+
+    pub fn init () -> (Delay, Pins) {
+        let mut peripherals = pac::Peripherals::take().unwrap();
+        let core = pac::CorePeripherals::take().unwrap();
+        // Set up the watchdog driver - needed by the clock setup code
+        let mut watchdog = Watchdog::new(peripherals.WATCHDOG);    
+        // The default is to generate a 125 MHz system clock
+        let clocks =  init_clocks_and_plls (
+                XTAL_FREQ_HZ,
+                peripherals.XOSC,
+                peripherals.CLOCKS,
+                peripherals.PLL_SYS,
+                peripherals.PLL_USB,
+                &mut peripherals.RESETS,
+                &mut watchdog,
+            )
+            .ok()
+            .unwrap();
+
+        let sio = Sio::new(peripherals.SIO);
+        let pins = Pins::new(peripherals.IO_BANK0,peripherals.PADS_BANK0,sio.gpio_bank0, &mut peripherals.RESETS);
+        // The delay object lets us wait for specified amounts of time (in milliseconds)
+        let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+        return (delay, pins);
+    }
+
+    pub fn setup_blink<'a> (d : &'a mut Delay, p : &'a mut Pin<Gpio25, PushPullOutput>) {
+        p.set_high().unwrap();
+        d.delay_ms(DELAY_LEN);
+        p.set_low().unwrap();
+        d.delay_ms(DELAY_LEN);
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
-use state_indicator::init;
+use state_indicator::state_model;
 use rp2040_hal::gpio::PushPullOutput;
 use rp2040_hal::gpio::bank0::Gpio25;
 use rp2040_hal::gpio::Pin;
-use embedded_hal::digital::v2::OutputPin;
 /// The linker will place this boot block at the start of our program image. We
 /// need this to help the ROM bootloader get our code up and running.
 /// Note: This boot block is not necessary when using a rp-hal based BSP
@@ -16,22 +15,13 @@ use embedded_hal::digital::v2::OutputPin;
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
-/// Entry point to our bare-metal application.
-///
-/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables and the spinlock are initialised.
-///
-const DELAY_LEN: u32 = 50;
-/// The function configures the RP2040 peripherals, then fades the LED in an
-/// infinite loop.
+
+
 #[rp2040_hal::entry]
 fn main() -> ! {
-    let (p, mut d) = init();
-    let mut pin25: Pin<Gpio25, PushPullOutput> = p.gpio25.into_mode();
+    let (mut delay, pins) = state_model::init();
+    let mut pin25: Pin<Gpio25, PushPullOutput> = pins.gpio25.into_mode();
     loop { 
-            pin25.set_high().unwrap();
-            d.delay_ms(DELAY_LEN);
-            pin25.set_low().unwrap();
-            d.delay_ms(DELAY_LEN);
-        }
+        state_model::setup_blink(&mut delay, &mut pin25);
+    }
 }
