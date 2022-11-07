e9073992b8be2bfb50e1c0b15bd47e870785d1ef
akilhylton <hyltonakil@gmail.com>
2022-11-07 18:46:33 -0500

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
+use rp2040_hal::{gpio::bank0::Gpio25, gpio::Pin, gpio::PushPullOutput};
 use state_indicator::state_model;
-use rp2040_hal::gpio::PushPullOutput;
-use rp2040_hal::gpio::bank0::Gpio25;
-use rp2040_hal::gpio::Pin;
 /// The linker will place this boot block at the start of our program image. We
 /// need this to help the ROM bootloader get our code up and running.
 /// Note: This boot block is not necessary when using a rp-hal based BSP
@@ -16,12 +14,11 @@ use rp2040_hal::gpio::Pin;
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
-
 #[rp2040_hal::entry]
 fn main() -> ! {
     let (mut delay, pins) = state_model::init();
     let mut pin25: Pin<Gpio25, PushPullOutput> = pins.gpio25.into_mode();
-    loop { 
+    loop {
         state_model::setup_blink(&mut delay, &mut pin25);
     }
 }
